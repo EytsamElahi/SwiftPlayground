5dd503de72314df5c28de710d5352e756773b075
Eytsam Elahi <eytsam.elahi555@gmail.com>
2025-12-09 19:28:25 +0500

    project created

---
+++ SwiftTesting/SwiftTesting/SwiftTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingApp.swift
+//  SwiftTesting
+//
+//  Created by eytsam elahi on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTesting/SwiftTestingTests/SwiftTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingTests.swift
+//  SwiftTestingTests
+//
+//  Created by eytsam elahi on 09/12/2025.
+//
+
+import Testing
+@testable import SwiftTesting
+
+struct SwiftTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
